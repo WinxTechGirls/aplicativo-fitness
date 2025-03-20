9743428135f0b2b251b702283b26ebb0c867f963
marianaiha <ihamariana@gmail.com>
2025-03-20 11:41:25 -0300

    primeiro commit

---
+++ src/main/java/com/generation/fitness/AplicativoFitnessApplication.java
@@ -0,0 +1,13 @@
+package com.generation.fitness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicativoFitnessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicativoFitnessApplication.class, args);
+	}
+
+}
