44970b60022fbea077b75fb6b1326ee6caf8ffcd
Mireli Borges <mika.oliveira1998@gmail.com>
2025-03-20 20:09:35 -0300

    Ajuste usuário

--- src/main/java/com/generation/fitness/model/Usuario.java
+++ src/main/java/com/generation/fitness/model/Usuario.java
@@ -1,16 +1,14 @@
 package com.generation.fitness.model;
 
-import java.util.List;
-
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
+
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.ManyToOne;
+
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -38,15 +36,15 @@ public class Usuario {
 
 	private String foto;
 
-	@OneToMany(fetch = FetchType.LAZY, mappedBy = "usuario", cascade = CascadeType.REMOVE)
+	@ManyToOne
 	@JsonIgnoreProperties("usuario")
-	private List<Produto> produto;
+	private Produto produto;
 
-	public List<Produto> getProduto() {
+	public Produto getProduto() {
 		return produto;
 	}
 
-	public void setProduto(List<Produto> produto) {
+	public void setProduto(Produto produto) {
 		this.produto = produto;
 	}
 
