a7e8e60f5982e85a83f77d89d4f9e5b1ab38508e
Mireli Borges <mika.oliveira1998@gmail.com>
2025-03-20 15:18:20 -0300

    Modificações da model

--- src/main/java/com/generation/fitness/model/Usuario.java
+++ src/main/java/com/generation/fitness/model/Usuario.java
@@ -1,16 +1,23 @@
 package com.generation.fitness.model;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 @Entity
-@Table(name = "usuarios")
+@Table(name = "tb_usuarios")
 public class Usuario {
 
 	@Id
@@ -30,6 +37,18 @@ public class Usuario {
 	private String senha;
 
 	private String foto;
+
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "usuario", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("usuario")
+	private List<Produto> produto;
+
+	public List<Produto> getProduto() {
+		return produto;
+	}
+
+	public void setProduto(List<Produto> produto) {
+		this.produto = produto;
+	}
 
 	public Long getId() {
 		return id;
